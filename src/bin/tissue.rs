dc3773b5fe9c0df66211e5929c6f8cddb2694140
Evie <14899090+eievui5@users.noreply.github.com>
2024-09-15 15:40:09 -0400

    TUI

--- src/bin/tissue.rs
+++ src/bin/tissue.rs
@@ -1,142 +1,118 @@
 use clap::{Args, Parser, Subcommand};
-use std::{collections::HashSet, fs, path::PathBuf, process::exit};
+use std::{fs, path::PathBuf, process::exit};
 use tracing::error;
 
 #[derive(Parser)]
 struct Cli {
-    #[clap(short, long, default_value = ".tissuebox")]
-    input: PathBuf,
-    #[command(subcommand)]
-    command: Option<Command>,
+	#[clap(short, long, default_value = ".tissuebox")]
+	input: PathBuf,
+	#[command(subcommand)]
+	command: Option<Command>,
 }
 
 #[derive(Subcommand)]
 enum Command {
-    /// Display formatted tissuebox
-    List,
-    /// Create new tissue
-    Add(AddArgs),
-    /// Append to an existing tissue's description by index
-    Describe(DescribeArgs),
-    /// Add a tag to an existing tissue by index
-    Tag(DescribeArgs),
-    /// Delete an existing tissue by index
-    Remove(RemoveArg),
+	/// Display formatted tissuebox
+	List,
+	/// Create new tissue
+	Add(AddArgs),
+	/// Append to an existing tissue's description by index
+	Describe(DescribeArgs),
+	/// Add a tag to an existing tissue by index
+	Tag(DescribeArgs),
+	/// Delete an existing tissue by index
+	Remove(RemoveArg),
 }
 
 #[derive(Args)]
 struct RemoveArg {
-    // index of tissue to delete
-    index: usize,
+	// index of tissue to delete
+	index: usize,
 }
 
 #[derive(Args)]
 struct AddArgs {
-    // title of tissue to create
-    title: String,
+	// title of tissue to create
+	title: String,
 }
 
 #[derive(Args)]
 struct DescribeArgs {
-    // index of tissue to describe
-    index: usize,
-    // description of tissue
-    with: String,
-}
-
-#[derive(Clone, Debug, Default, serde::Serialize, serde::Deserialize)]
-pub struct Tissue {
-    pub title: String,
-    #[serde(default)]
-    pub description: Vec<String>,
-    #[serde(default)]
-    pub tags: HashSet<String>,
-}
-
-#[derive(Clone, Debug, Default, serde::Serialize, serde::Deserialize)]
-pub struct TissueBox {
-    #[serde(default)]
-    tissues: Vec<Tissue>,
+	// index of tissue to describe
+	index: usize,
+	// description of tissue
+	with: String,
 }
 
 fn main() {
-    tracing_subscriber::fmt::init();
-    let cli = Cli::parse();
+	tracing_subscriber::fmt::init();
+	let cli = Cli::parse();
 
-    // Load tissue box
-    let tissue_box_toml = fs::read_to_string(&cli.input).unwrap_or_else(|msg| {
-        error!("failed to read {}: {msg}", cli.input.display());
-        exit(1);
-    });
-    let mut tissue_box: TissueBox = toml::from_str(&tissue_box_toml).unwrap_or_else(|msg| {
-        error!(
-            "failed to parse {} as tissue box: {msg}",
-            cli.input.display()
-        );
-        exit(1);
-    });
+	// Load tissue box
+	let tissue_box_toml = fs::read_to_string(&cli.input).unwrap_or_else(|msg| {
+		error!("failed to read {}: {msg}", cli.input.display());
+		exit(1);
+	});
+	let mut tissue_box: tissuebox::Box = toml::from_str(&tissue_box_toml).unwrap_or_else(|msg| {
+		error!(
+			"failed to parse {} as tissue box: {msg}",
+			cli.input.display()
+		);
+		exit(1);
+	});
 
-    // Update tissue box
-    match cli.command {
-        Some(Command::List) => {
-            for (
-                index,
-                Tissue {
-                    title,
-                    description,
-                    tags,
-                },
-            ) in tissue_box.tissues.iter().enumerate()
-            {
-                print!("{index}. {title}");
-                if !tags.is_empty() {
-                    print!(
-                        " ({})",
-                        tags.iter().cloned().collect::<Vec<String>>().join(", ")
-                    );
-                }
-                println!();
-                for description in description {
-                    println!("\t- {description}");
-                }
-            }
-        }
-        Some(Command::Add(AddArgs { title })) => {
-            tissue_box.tissues.push(Tissue {
-                title,
-                ..Default::default()
-            });
-        }
-        Some(Command::Describe(DescribeArgs { index, with })) => {
-            let Some(tissue) = tissue_box.tissues.get_mut(index) else {
-                error!("no tissue with index {index}");
-                exit(1);
-            };
-            tissue.description.push(with);
-        }
-        Some(Command::Tag(DescribeArgs { index, with })) => {
-            let Some(tissue) = tissue_box.tissues.get_mut(index) else {
-                error!("no tissue with index {index}");
-                exit(1);
-            };
-            tissue.tags.insert(with);
-        }
-        Some(Command::Remove(RemoveArg { index })) => {
-            let Some(_tissue) = tissue_box.tissues.get(index) else {
-                error!("no tissue with index {index}");
-                exit(1);
-            };
-            tissue_box.tissues.remove(index);
-        }
-        None => todo!(),
-    }
+	// Update tissue box
+	match cli.command {
+		Some(Command::List) => print!("{tissue_box}"),
+		Some(Command::Add(AddArgs { title })) => tissue_box.create(title),
+		Some(Command::Describe(DescribeArgs { index, with })) => {
+			let Some(tissue) = tissue_box.get_mut(index) else {
+				error!("no tissue with index {index}");
+				exit(1);
+			};
+			tissue.description.push(with);
+		}
+		Some(Command::Tag(DescribeArgs { index, with })) => {
+			let Some(tissue) = tissue_box.get_mut(index) else {
+				error!("no tissue with index {index}");
+				exit(1);
+			};
+			tissue.tags.insert(with);
+		}
+		Some(Command::Remove(RemoveArg { index })) => {
+			if tissue_box.remove(index).is_none() {
+				error!("no tissue with index {index}");
+				exit(1);
+			};
+		}
+		None => {
+			{
+				use crossterm::execute;
+				use crossterm::terminal::disable_raw_mode;
+				use std::panic;
 
-    // Save tissue box
-    let tissue_box_toml = toml::to_string(&tissue_box).unwrap_or_else(|msg| {
-        error!("failed to serialize tissue box: {msg}");
-        exit(1);
-    });
-    fs::write(&cli.input, tissue_box_toml).unwrap_or_else(|msg| {
-        error!("failed to write tissue box: {msg}");
-    });
+				let original_hook = panic::take_hook();
+				panic::set_hook(Box::new(move |panic_info| {
+					// intentionally ignore errors here since we're already in a panic
+
+					let _ = disable_raw_mode();
+					let _ = execute!(std::io::stdout(), crossterm::terminal::LeaveAlternateScreen);
+					original_hook(panic_info);
+				}));
+			}
+			if let Err(msg) = tissuebox::tui::run(&mut tissue_box) {
+				error!("{msg}");
+				exit(1);
+			}
+		}
+	}
+
+	// Save tissue box
+	let tissue_box_toml = toml::to_string(&tissue_box).unwrap_or_else(|msg| {
+		error!("failed to serialize tissue box: {msg}");
+		exit(1);
+	});
+	fs::write(&cli.input, tissue_box_toml).unwrap_or_else(|msg| {
+		error!("failed to write tissue box: {msg}");
+	});
 }
---
+++ src/lib.rs
@@ -0,0 +1,338 @@
+use std::collections::HashSet;
+
+#[derive(Clone, Debug, Default, serde::Serialize, serde::Deserialize)]
+pub struct Tissue {
+	pub title: String,
+	#[serde(default)]
+	pub description: Vec<String>,
+	#[serde(default)]
+	pub tags: HashSet<String>,
+}
+
+impl std::fmt::Display for Tissue {
+	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+		let Tissue {
+			title,
+			description,
+			tags,
+		} = self;
+		write!(f, "{title}")?;
+		if !tags.is_empty() {
+			write!(
+				f,
+				" ({})",
+				tags.iter().cloned().collect::<Vec<String>>().join(", ")
+			)?;
+		}
+		writeln!(f)?;
+		for description in description {
+			writeln!(f, "  - {description}")?;
+		}
+		Ok(())
+	}
+}
+
+#[derive(Clone, Debug, Default, serde::Serialize, serde::Deserialize)]
+pub struct Box {
+	#[serde(default)]
+	tissues: Vec<Tissue>,
+}
+
+impl Box {
+	pub fn create(&mut self, title: String) {
+		self.tissues.push(Tissue {
+			title,
+			..Default::default()
+		})
+	}
+
+	#[must_use]
+	pub fn remove(&mut self, index: usize) -> Option<Tissue> {
+		self.tissues.get(index)?;
+		Some(self.tissues.remove(index))
+	}
+
+	pub fn get(&self, index: usize) -> Option<&Tissue> {
+		self.tissues.get(index)
+	}
+
+	pub fn get_mut(&mut self, index: usize) -> Option<&mut Tissue> {
+		self.tissues.get_mut(index)
+	}
+}
+
+impl std::fmt::Display for Box {
+	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+		for (index, tissue) in self.tissues.iter().enumerate() {
+			write!(f, "{index}. {tissue}")?;
+		}
+		Ok(())
+	}
+}
+
+pub mod tui {
+	use crossterm::event::{self, KeyCode, KeyEventKind};
+	use ratatui::{
+		layout::{Alignment, Offset, Rect},
+		style::Stylize,
+		symbols::border,
+		text::{Line, Text},
+		widgets::{
+			block::{Position, Title},
+			Block, Padding, Paragraph,
+		},
+		DefaultTerminal,
+	};
+	use std::io;
+
+	pub fn run(tissue_box: &mut crate::Box) -> io::Result<()> {
+		let mut terminal = ratatui::init();
+		terminal.clear()?;
+		let result = tui(terminal, tissue_box);
+		ratatui::restore();
+		result
+	}
+
+	fn tui(mut terminal: DefaultTerminal, tissue_box: &mut crate::Box) -> io::Result<()> {
+		fn gather_line(line: &mut String, code: KeyCode) -> bool {
+			match code {
+				KeyCode::Backspace => {
+					line.pop();
+				}
+				KeyCode::Enter => return true,
+				KeyCode::Char(c) => line.push(c),
+				_ => {}
+			}
+			false
+		}
+
+		enum Mode {
+			Normal,
+			Add(String),
+			Describe(String),
+			Tag(String),
+			Copy,
+			Remove,
+			RemoveDescription(usize),
+			RemoveTag(String),
+		}
+		let mut index = 0usize;
+		let mut mode = Mode::Normal;
+		loop {
+			index = index.min(tissue_box.tissues.len() - 1);
+			terminal.draw(|frame| {
+				let area = frame.area();
+				let title = Title::from(" Tissue Box ".red().bold());
+				let instructions = match &mode {
+					Mode::Normal => {
+						Title::from(Line::from(vec![
+							" a".red().bold(),
+							"dd".into(),
+							" d".red().bold(),
+							"escribe".into(),
+							" t".red().bold(),
+							"ag".into(),
+							" c".red().bold(),
+							"opy".into(),
+							" r".red().bold(),
+							"emove".into(),
+							" q".red().bold(),
+							"uit ".into(),
+						]))
+					},
+					Mode::Add(title) => Title::from(Line::from(vec![
+						" Add tissue: ".blue().bold(),
+						title.into(),
+						"_ ".into(),
+					])),
+					Mode::Describe(description) => Title::from(Line::from(vec![
+						" Describe tissue: ".blue().bold(),
+						description.into(),
+						"_ ".into(),
+					])),
+					Mode::Tag(tag) => Title::from(Line::from(vec![
+						" Tag tissue: ".blue().bold(),
+						tag.into(),
+						"_ ".into(),
+					])),
+					Mode::Copy => {
+						Title::from(Line::from(vec![
+							" Copy what?:".blue().bold(),
+							" t".red().bold(),
+							"itle".into(),
+							" d".red().bold(),
+							"escription".into(),
+							" l".red().bold(),
+							"ist".into(),
+						]))
+					}
+					Mode::Remove => {
+						Title::from(Line::from(vec![
+							" Remove what?:".blue().bold(),
+							" T".red().bold(),
+							"issue".into(),
+							" d".red().bold(),
+							"escription".into(),
+							" t".red().bold(),
+							"ag".into(),
+						]))
+					}
+					Mode::RemoveDescription(_) => Title::from(Line::from(vec![
+						" Remove which description? ".blue().bold(),
+					])),
+					Mode::RemoveTag(tag) => Title::from(Line::from(vec![
+						" Remove tag: ".blue().bold(),
+						tag.into(),
+						"_ ".into(),
+					])),
+				};
+				let block = Block::bordered()
+					.title(title.alignment(Alignment::Center))
+					.title(
+						instructions
+							.alignment(Alignment::Center)
+							.position(Position::Bottom),
+					)
+					.padding(Padding::horizontal(2))
+					.border_set(border::ROUNDED);
+
+				let paper = Paragraph::new(
+					"  ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓\n ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓\n▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓\n▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓\n",
+				)
+				.centered();
+				frame.render_widget(paper, Rect { height: 4, ..area });
+				let mut body = Text::default();
+				for (i, tissue) in tissue_box.tissues.iter().enumerate() {
+					let mut title: Line = tissue.title.clone().into();
+					if let Mode::RemoveDescription(_) = mode {} else if i == index{
+						title = title.black().on_white();
+					};
+					for tag in &tissue.tags {
+						title.spans.push(format!(" ({tag})").magenta());
+					}
+					body.lines.push(title);
+					for (i, description) in tissue.description.iter().enumerate() {
+						if let Mode::RemoveDescription(index) = mode {
+							if index == i {
+								body.lines.push(format!("- {description}").black().on_white().into());
+								continue;
+							}
+						}
+						body.lines.push(format!("- {description}").dark_gray().into());
+					}
+				}
+				frame.render_widget(Paragraph::new(body).block(block), area.offset(Offset { x: 0, y: 4 }));
+			})?;
+
+			if let event::Event::Key(key) = event::read()? {
+				if key.kind == KeyEventKind::Press {
+					if key.code == KeyCode::Esc {
+						mode = Mode::Normal;
+					} else {
+						mode = match mode {
+							Mode::Normal => match key.code {
+								KeyCode::Char('k')
+								| KeyCode::Char('h')
+								| KeyCode::Up
+								| KeyCode::Left => {
+									index = index.saturating_sub(1);
+									Mode::Normal
+								}
+								KeyCode::Char('j')
+								| KeyCode::Char('l')
+								| KeyCode::Down
+								| KeyCode::Right => {
+									index += 1;
+									Mode::Normal
+								}
+								KeyCode::Char('a') => Mode::Add(String::new()),
+								KeyCode::Char('d') => Mode::Describe(String::new()),
+								KeyCode::Char('t') => Mode::Tag(String::new()),
+								KeyCode::Char('c') => Mode::Copy,
+								KeyCode::Char('r') => Mode::Remove,
+								KeyCode::Char('q') => return Ok(()),
+								KeyCode::Char('Q') => panic!("force quit"),
+								_ => Mode::Normal,
+							},
+							Mode::Add(mut title) => {
+								if gather_line(&mut title, key.code) {
+									// This clone is uneccessary.
+									// A move should be possible since it's destroyed imeediately afterwards anyways.
+									tissue_box.create(title);
+									Mode::Normal
+								} else {
+									Mode::Add(title)
+								}
+							}
+							Mode::Describe(mut description) => {
+								if gather_line(&mut description, key.code) {
+									tissue_box.tissues[index].description.push(description);
+									Mode::Normal
+								} else {
+									Mode::Describe(description)
+								}
+							}
+							Mode::Tag(mut tag) => {
+								if gather_line(&mut tag, key.code) {
+									tissue_box.tissues[index].tags.insert(tag);
+									Mode::Normal
+								} else {
+									Mode::Tag(tag)
+								}
+							}
+							Mode::Copy => match key.code {
+								KeyCode::Char('t') => todo!(),
+								KeyCode::Char('d') => todo!(),
+								KeyCode::Char('l') => todo!(),
+								_ => Mode::Copy,
+							},
+							Mode::Remove => match key.code {
+								KeyCode::Char('T') => {
+									tissue_box.tissues.remove(index);
+									Mode::Normal
+								}
+								KeyCode::Char('d') => {
+									if tissue_box.tissues[index].description.is_empty() {
+										Mode::Normal
+									} else {
+										Mode::RemoveDescription(0)
+									}
+								}
+								KeyCode::Char('t') => Mode::RemoveTag(String::new()),
+								_ => Mode::Remove,
+							},
+							Mode::RemoveDescription(i) => {
+								let tissue = &mut tissue_box.tissues[index];
+								match key.code {
+									KeyCode::Char('k')
+									| KeyCode::Char('h')
+									| KeyCode::Up
+									| KeyCode::Left => Mode::RemoveDescription(i.saturating_sub(1)),
+									KeyCode::Char('j')
+									| KeyCode::Char('l')
+									| KeyCode::Down
+									| KeyCode::Right => Mode::RemoveDescription(
+										(i + 1).min(tissue.description.len() - 1),
+									),
+									KeyCode::Enter => {
+										tissue.description.remove(i);
+										Mode::Normal
+									}
+									_ => Mode::RemoveDescription(i),
+								}
+							}
+							Mode::RemoveTag(mut tag) => {
+								if gather_line(&mut tag, key.code) {
+									tissue_box.tissues[index].tags.remove(&tag);
+									Mode::Normal
+								} else {
+									Mode::RemoveTag(tag)
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+}
