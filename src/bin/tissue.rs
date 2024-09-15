5bfe9d6e1e32cc84b05cb9ddd240cdc1b440d50a
Evie <14899090+eievui5@users.noreply.github.com>
2024-09-15 09:48:50 -0400

    Reformat serialization format (annoying)

--- src/bin/tissue.rs
+++ src/bin/tissue.rs
@@ -1,10 +1,5 @@
 use clap::{Args, Parser, Subcommand};
-use std::{
-    collections::{HashMap, HashSet},
-    fs,
-    path::PathBuf,
-    process::exit,
-};
+use std::{collections::HashSet, fs, path::PathBuf, process::exit};
 use tracing::error;
 
 #[derive(Parser)]
@@ -51,8 +46,17 @@ struct DescribeArgs {
 
 #[derive(Clone, Debug, Default, serde::Serialize, serde::Deserialize)]
 pub struct Tissue {
+    pub title: String,
+    #[serde(default)]
+    pub description: Vec<String>,
+    #[serde(default)]
     pub tags: HashSet<String>,
-    pub description: String,
+}
+
+#[derive(Clone, Debug, Default, serde::Serialize, serde::Deserialize)]
+pub struct TissueBox {
+    #[serde(default)]
+    tissues: Vec<Tissue>,
 }
 
 fn main() {
@@ -64,65 +68,65 @@ fn main() {
         error!("failed to read {}: {msg}", cli.input.display());
         exit(1);
     });
-    let mut tissue_box: HashMap<String, Tissue> =
-        toml::from_str(&tissue_box_toml).unwrap_or_else(|msg| {
-            error!(
-                "failed to parse {} as tissue box: {msg}",
-                cli.input.display()
-            );
-            exit(1);
-        });
+    let mut tissue_box: TissueBox = toml::from_str(&tissue_box_toml).unwrap_or_else(|msg| {
+        error!(
+            "failed to parse {} as tissue box: {msg}",
+            cli.input.display()
+        );
+        exit(1);
+    });
 
     // Update tissue box
     match cli.command {
         Some(Command::List) => {
-            let mut tissues: Vec<_> = tissue_box.iter_mut().collect();
-            tissues.sort_unstable_by(|a, b| a.0.cmp(b.0));
-            for (index, (tissue, Tissue { tags, description })) in tissues.into_iter().enumerate() {
-                let tags = tags.iter().cloned().collect::<Vec<String>>().join(", ");
-                print!("{index}. {tissue}");
+            for (
+                index,
+                Tissue {
+                    title,
+                    description,
+                    tags,
+                },
+            ) in tissue_box.tissues.iter().enumerate()
+            {
+                print!("{index}. {title}");
                 if !tags.is_empty() {
-                    print!(" ({tags})");
+                    print!(
+                        " ({})",
+                        tags.iter().cloned().collect::<Vec<String>>().join(", ")
+                    );
                 }
                 println!();
-                if !description.is_empty() {
+                for description in description {
                     println!("\t- {description}");
                 }
             }
         }
         Some(Command::Add(AddArgs { title })) => {
-            tissue_box.insert(title, Tissue::default());
+            tissue_box.tissues.push(Tissue {
+                title,
+                ..Default::default()
+            });
         }
         Some(Command::Describe(DescribeArgs { index, with })) => {
-            let mut tissues: Vec<_> = tissue_box.iter_mut().collect();
-            tissues.sort_unstable_by(|a, b| a.0.cmp(b.0));
-            let (_, tissue) = tissues.get_mut(index).unwrap_or_else(|| {
+            let Some(tissue) = tissue_box.tissues.get_mut(index) else {
                 error!("no tissue with index {index}");
                 exit(1);
-            });
-            tissue.description += &with;
+            };
+            tissue.description.push(with);
         }
         Some(Command::Tag(DescribeArgs { index, with })) => {
-            let mut tissues: Vec<_> = tissue_box.iter_mut().collect();
-            tissues.sort_unstable_by(|a, b| a.0.cmp(b.0));
-            let (_, tissue) = tissues.get_mut(index).unwrap_or_else(|| {
+            let Some(tissue) = tissue_box.tissues.get_mut(index) else {
                 error!("no tissue with index {index}");
                 exit(1);
-            });
+            };
             tissue.tags.insert(with);
         }
         Some(Command::Remove(RemoveArg { index })) => {
-            let mut tissues: Vec<_> = tissue_box.iter_mut().collect();
-            tissues.sort_unstable_by(|a, b| a.0.cmp(b.0));
-            let tissue = tissues
-                .get_mut(index)
-                .unwrap_or_else(|| {
-                    error!("no tissue with index {index}");
-                    exit(1);
-                })
-                .0
-                .clone();
-            tissue_box.remove(tissue.as_str());
+            let Some(_tissue) = tissue_box.tissues.get(index) else {
+                error!("no tissue with index {index}");
+                exit(1);
+            };
+            tissue_box.tissues.remove(index);
         }
         None => todo!(),
     }
